eb7f25900433f8fadcb6e33c0e93b017a5e23911
Andrei C. <42174110+soitirakis@users.noreply.github.com>
2025-01-21 20:24:39 +0100

    Create credit.c

---
+++ week2/Credit/credit.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{
+    long userCard = get_str("Enter card number: ");
+    //check the length to be between 13 and 16
+    printf(userCard);
+}
